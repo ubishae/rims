82ab9ff09284af4bafc6037eec3a98b2fd0a06fd
Ahmed Bashir <bashir@0g.ae>
2025-01-29 17:24:34 +0400

    feat: add tanstack query

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
 // Import the generated route tree
@@ -16,13 +17,18 @@ declare module '@tanstack/react-router' {
   }
 }
 
+// Create a client
+const queryClient = new QueryClient()
+
 // Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
